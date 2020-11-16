9f36fb90046047cf1d48cfaed8f55af6fa4f388b
Jim Minter <jminter@redhat.com>
2020-11-16 11:16:53 -0600

    CARRY: HACK: create worker registries machineconfig very early

--- pkg/asset/manifests/template.go
+++ pkg/asset/manifests/template.go
@@ -80,6 +80,7 @@ type bootkubeTemplateData struct {
 	PullSecretBase64           string
 	RootCaCert                 string
 	WorkerIgnConfig            string
+	AROWorkerRegistries        string
 }
 
 type baremetalTemplateData struct {
